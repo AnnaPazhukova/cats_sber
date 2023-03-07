b15d6983d1747df5b34c44450d27f7efb548b2d1
AnnaPazhukova <91010809+AnnaPazhukova@users.noreply.github.com>
2023-03-07 14:42:54 +0300

    start_of_programming

---
+++ js/cats.js
@@ -0,0 +1,65 @@
+const cats = [
+    {
+        "id":2,
+        "name":"Ласкун",
+        "rate":5,
+        "age":2,
+        "favorite":true,
+        "description":"Безумно любит обниматься",
+        "image":"https://legkovmeste.ru/wp-content/uploads/2019/02/post_5b76fabe77f52.jpg"
+    },
+    {
+        "id":3,
+        "name":"Мурка",
+        "rate":8,
+        "age":3,
+        "favorite":true,
+        "description":"Ест мышей",
+        "image":"https://legkovmeste.ru/wp-content/uploads/2019/02/post_5b76fabe77f52.jpg"
+    },
+    {
+        "id":5,
+        "name":"Барсик",
+        "rate":6,
+        "age":5,
+        "favorite":true,
+        "description":"Очень ласковый",
+        "image":"https://legkovmeste.ru/wp-content/uploads/2019/02/post_5b76fabe77f52.jpg"
+    },
+    {
+        "id":6,
+        "name":"Мурка",
+        "rate":5,
+        "age":6,
+        "favorite":true,
+        "description":"Красивая и дружелюбная",
+        "image":"https://legkovmeste.ru/wp-content/uploads/2019/02/post_5b76fabe77f52.jpg"
+    },
+    {
+        "id":7,
+        "name":"Свинтус",
+        "rate":4,
+        "age":7,
+        "favorite":true,
+        "description":"Неаккуратно кушает",
+        "image":"https://legkovmeste.ru/wp-content/uploads/2019/02/post_5b76fabe77f52.jpg"
+    },
+    {
+        "id":8,
+        "name":"Жора",
+        "rate":3,
+        "age":8,
+        "favorite":true,
+        "description":"Большой, добрый и толстенький",
+        "image":"https://legkovmeste.ru/wp-content/uploads/2019/02/post_5b76fabe77f52.jpg"
+    },
+    {
+        "id":9,
+        "name":"Софийка",
+        "rate":2,
+        "age":9,
+        "favorite":true,
+        "description":"Шкодничает и не даёт хозяину спокойно",
+        "image":"https://legkovmeste.ru/wp-content/uploads/2019/02/post_5b76fabe77f52.jpg"
+    }
+];
---
+++ js/popup.js
@@ -0,0 +1,26 @@
+class Popup {
+    #handleEscUp = (evt) => {
+        if(evt.key === 'Escape'){
+            this.close();
+        }
+    }
+    constructor(classPopup){
+        this._popupElement = document.querySelector(`.${classPopup}`);
+    }
+
+    open(){
+        this._popupElement.classList.add(`popup_active`);
+        document.addEventListener('keyup', this.#handleEscUp);
+    }
+    close(){
+        this._popupElement.classList.remove(`popup_active`);
+        document.removeEventListener('keyup', this.#handleEscUp);
+    }
+    setEventListener(){
+        this._popupElement.addEventListener('mousedown', evt => {
+            if(evt.target.classList.contains ('popup') || Boolean.evt.target.classList.closest('.popup__close')){
+                this.close()
+            }
+        })
+    }
+}
